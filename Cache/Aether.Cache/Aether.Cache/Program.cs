ba8b71ab7bcdf0e89fa0fdcc8412061cfeb8e69d
Ryan G <33613258+kuzann@users.noreply.github.com>
2025-02-26 21:38:36 +0700

    Added cache and fix events

---
+++ Cache/Aether.Cache/Aether.Cache/Program.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace Aether.Cache
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+
+            db.StringSet("foo", "bar");
+            Console.WriteLine(db.StringGet("foo"));
+        }
+    }
+}
--- Events/Aether.EventsAndDelegates/Aether.EventsAndDelegates/Program.cs
+++ Events/Aether.EventsAndDelegates/Aether.EventsAndDelegates/Program.cs
@@ -4,32 +4,20 @@
     {
         static void Main(string[] args)
         {
-            //var video = new Video()
-            //{
-            //    Title = "Video 1"
-            //};
-            //var videoEncoder = new VideoEncoder(); // publisher
-            //var mailService = new MailService(); // subscriber
-            //var messageService = new MessageService(); // subscriber
+            var video = new Video()
+            {
+                Title = "Video 1"
+            };
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService(); // subscriber
+            var messageService = new MessageService(); // subscriber
 
-            //videoEncoder.VideoEncoded += mailService.OnVideoEncoded; // subscribe
-            //videoEncoder.VideoEncoded += messageService.OnVideoEncoded; // subscribe
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded; // subscribe
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded; // subscribe
 
-            //videoEncoder.Encode(video);
+            videoEncoder.Encode(video);
 
-            Console.WriteLine(NumberOfWays(842, 91)); //143119619
-        }
-
-        public static int NumberOfWays(int N, int K)
-        {
-            // Base case
-            if (N == 0)
-                return 1;
-            if (N < 0 || K <= 0)
-                return 0;
-
-            // including and not including K in sum
-            return NumberOfWays(N - K, K) + NumberOfWays(N, K - 1);
+            Console.WriteLine("End of program");
         }
     }
 }
