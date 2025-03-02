4fe4d8709a83b972ac5f8af24135e02f935d2b45
Ryan G <33613258+kuzann@users.noreply.github.com>
2025-03-02 14:48:14 +0700

    init k8s cron sample project

---
+++ KubernetesCron/Aether.Kubernetes.Cron/Program.cs
@@ -0,0 +1,14 @@
+namespace Aether.Kubernetes.Cron
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddHostedService<Worker>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
